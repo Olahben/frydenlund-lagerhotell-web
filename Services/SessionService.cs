44afd1ae56c57b6c6acdfb3bf6e884613e6570c3
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-02 18:39:12 +0100

    Add ctor, method AddJwtToLocalStorage

---
+++ Services/SessionService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace Lagerhotell.Services.UserService
+{
+    public class SessionService
+    {
+        private readonly IJSRuntime jsRuntime;
+        public SessionService(IJSRuntime jsRuntime)
+        {
+            this.jsRuntime = jsRuntime;
+        }
+        public async Task? AddJwtToLocalStorage(string jwt)
+        {
+            await jsRuntime.InvokeVoidAsync("localStorage.setItem", "jwtToken", jwt);
+        }
+    }
+}
