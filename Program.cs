56a86e27ccfee9c0d39a5e07c24753a7e658fb94
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-19 13:05:37 +0100

    Add AuthorizationCore, types to scope

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Lagerhotell;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -10,5 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddScoped<Lagerhotell.Services.UserService.SessionService>();
+builder.Services.AddScoped<AuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();
