507b58421b06d226b608b2c823f930ddb43ebefa
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-19 13:07:18 +0100

    Add RemoveItemAsync

--- Services/SessionService.cs
+++ Services/SessionService.cs
@@ -4,20 +4,34 @@ namespace Lagerhotell.Services.UserService
 {
     public class SessionService
     {
-        private readonly IJSRuntime jsRuntime;
+        private readonly string _baseUrl = "https://localhost:7272/users";
+        private readonly IJSRuntime _jsRuntime;
         public SessionService(IJSRuntime jsRuntime)
         {
-            this.jsRuntime = jsRuntime;
+            this._jsRuntime = jsRuntime;
         }
         public async Task? AddJwtToLocalStorage(string jwt)
         {
-            await jsRuntime.InvokeVoidAsync("localStorage.setItem", "jwtToken", jwt);
+            await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "jwtToken", jwt);
         }
 
         public async Task<string> GetJwtFromLocalStorage()
         {
-            var token = await jsRuntime.InvokeAsync<string>("localStorage.getItem", "jwtToken");
-            return token ?? throw new Exception("Brukeren har ingen JWT");
+            var token = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "jwtToken");
+            return token ?? null;
         }
+
+        /*public async Task<string> GetTokenPhoneNumber(string token)
+        {
+            DecodeJwt.DecodeJwtRequest request = new() { Token = token };
+            string url = _baseUrl + "/decode-token";
+            return "";
+        } */
+
+        public async Task RemoveItemAsync(string key)
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", key);
+        }
+
     }
 }
