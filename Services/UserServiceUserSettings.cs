7e453dcb9f85e2214cc1a69fa06be299df3bb113
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-29 15:02:35 +0100

    Add function UpdateUserValues

--- Services/UserServiceUserSettings.cs
+++ Services/UserServiceUserSettings.cs
@@ -1,13 +1,14 @@
 ﻿using LagerhotellAPI.Models;
 using System.Text;
 using System.Text.Json;
+using static Lagerhotell.Pages.UserSettings.UserSettings;
 
 namespace Lagerhotell.Services.UserService
 {
     public class UserServiceUserSettings
     {
         private readonly string _baseUrl = "https://localhost:7272/users";
-        public async Task<User> LoadUser(HttpClient client, User user, string userId, string jwtToken)
+        public async Task<UserValues> LoadUser(HttpClient client, UserValues user, string userId, string jwtToken)
         {
             string url = _baseUrl + "/get-user";
             var request = new LagerhotellAPI.Models.GetUserRequest { UserId = userId };
@@ -23,7 +24,7 @@ namespace Lagerhotell.Services.UserService
                 {
                     PropertyNameCaseInsensitive = true
                 };
-                LagerhotellAPI.Models.User deserializedUser = JsonSerializer.Deserialize<LagerhotellAPI.Models.User>(result, options);
+                UserValues deserializedUser = JsonSerializer.Deserialize<UserValues>(result, options);
                 Console.WriteLine(deserializedUser);
                 user.FirstName = deserializedUser.FirstName;
                 user.LastName = deserializedUser.LastName;
@@ -39,6 +40,25 @@ namespace Lagerhotell.Services.UserService
             {
                 Console.WriteLine($"Error: {response.StatusCode}");
                 return user;
+            }
+        }
+
+        public async Task<bool>? UpdateUserValues(HttpClient httpClient, UserValues userValues, string jwtToken)
+        {
+            string url = _baseUrl + "/update-user-values";
+            UpdateUserValuesRequest request = new UpdateUserValuesRequest { FirstName = userValues.FirstName, LastName = userValues.LastName, PhoneNumber = userValues.PhoneNumber, BirthDate = userValues.BirthDate, Address = userValues.Address, PostalCode = userValues.PostalCode, City = userValues.City, Password = userValues.Password };
+            string jsonData = JsonSerializer.Serialize(request);
+            StringContent stringContentRequest = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", jwtToken);
+
+            HttpResponseMessage response = await httpClient.PostAsync(url, stringContentRequest);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Brukeren eksisterer ikke");
             }
         }
     }
