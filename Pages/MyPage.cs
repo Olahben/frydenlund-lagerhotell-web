d5f45f7c860da1e74d30e155dff2c128d09c0f61
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-03-01 14:33:03 +0100

    Enable universal access for namespaces

---
+++ Pages/MyPage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Lagerhotell.Pages
+{
+    public class MyPage : ComponentBase
+    {
+        [Inject]
+        protected ISnackbar Snackbar { get; set; }
+        [Inject]
+        protected IDialogService DialogService { get; set; }
+
+    }
+}
