25bfd1616bce9e003229e9bd83cf4acadda1227a
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-04-05 14:52:09 +0200

    Create

---
+++ Services/TextHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Lagerhotell.Services;
+
+public class TextHandler
+{
+
+    /// <summary>
+    /// Parses a string into paragraphs
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns>list of paragraphs</returns>
+    public List<string> ParseParagraphs(string text)
+    {
+        List<string> paragraphs = new List<string>();
+        string[] lines = text.Split("\n\n");
+        foreach (string line in lines)
+        {
+            paragraphs.Add(line);
+        }
+        return paragraphs;
+    }
+}
