331c5aa26d5ca6e8372248ef17d9926d397f57e2
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-04-05 14:51:22 +0200

    Add summary to methods

--- Services/FileHandler.cs
+++ Services/FileHandler.cs
@@ -4,6 +4,12 @@ namespace Lagerhotell.Services
 {
     public class FileHandler
     {
+
+        /// <summary>
+        /// Converts a IBrowserFile to a byte array
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>byte array</returns>
         public async Task<byte[]> ConvertToByteArray(IBrowserFile file)
         {
             using (var memoryStream = new MemoryStream())
