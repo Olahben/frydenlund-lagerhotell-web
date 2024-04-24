357ed924c177bf305e34c17fc27aa10e41fa7f88
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-04-24 18:57:58 +0200

    Add method "GetWarehouseHotelImagecarouselImages"

--- Services/AssetService.cs
+++ Services/AssetService.cs
@@ -4,4 +4,27 @@ public class AssetService
 {
     private readonly HttpClient client = new();
     private readonly string _baseUrl = "https://localhost:7272/assets";
+
+    /// <summary>
+    /// Gets all linked assets to a warehouse hotel
+    /// </summary>
+    /// <param name="warehouseHotelId"></param>
+    /// <returns>List of assets converted to base64 strings</returns>
+    public async Task<List<string>> GetWarehouseHotelImagecarouselImages(string warehouseHotelId)
+    {
+        string url = _baseUrl + $"/0/0/{warehouseHotelId}";
+        HttpResponseMessage response = await client.GetAsync(url);
+        string responseString = await response.Content.ReadAsStringAsync();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        List<ImageAsset> responseImageAssets = JsonSerializer.Deserialize<GetAllAssetsResponse>(responseString, options).Assets;
+        List<string> imagesBase64 = new();
+        foreach (ImageAsset imageAsset in responseImageAssets)
+        {
+            imagesBase64.Add(Convert.ToBase64String(imageAsset.ImageBytes));
+        }
+        return imagesBase64;
+    }
 }
