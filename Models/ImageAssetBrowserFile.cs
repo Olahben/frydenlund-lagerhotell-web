707691a2065bf9f430f2e528c21e9e84bddb7f14
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-04-26 12:29:23 +0200

    Change maxAllowedSize

---
+++ Models/ImageAssetBrowserFile.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Lagerhotell.Models;
+
+public class ImageAssetBrowserFile : IBrowserFile
+{
+    private readonly ImageAsset _imageAsset;
+
+    public ImageAssetBrowserFile(ImageAsset imageAsset)
+    {
+        _imageAsset = imageAsset;
+        Name = imageAsset.Name;
+        LastModified = DateTimeOffset.Now;
+        Size = imageAsset.ImageBytes.LongLength;
+        ContentType = "image/jpeg";
+    }
+
+    public string Name { get; private set; }
+    public DateTimeOffset LastModified { get; private set; }
+    public long Size { get; private set; }
+    public string ContentType { get; private set; }
+
+    public Stream OpenReadStream(long maxAllowedSize = 5000000, CancellationToken cancellationToken = default)
+    {
+        if (Size > maxAllowedSize)
+        {
+            throw new InvalidOperationException($"The file size exceeds the allowed limit of {maxAllowedSize} bytes.");
+        }
+        cancellationToken.ThrowIfCancellationRequested();
+        return new MemoryStream(_imageAsset.ImageBytes);
+    }
+}
