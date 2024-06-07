1df887c6c03ca457e9634cf4aae39cf48fb5d239
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-06-07 10:23:32 +0200

    Run "GetAuthenticationState" on every component render

--- Pages/MyPage.cs
+++ Pages/MyPage.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Lagerhotell.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
 
 namespace Lagerhotell.Pages
@@ -11,6 +13,24 @@ namespace Lagerhotell.Pages
         protected IDialogService DialogService { get; set; }
         [Inject]
         protected NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public SessionService SessionService { get; set; }
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            var userJwt = await SessionService.GetJwtFromLocalStorage();
+            if (userJwt != null)
+            {
+                var authState = await AuthenticationStateProvider
+            .GetAuthenticationStateAsync();
+                var user = authState.User;
+                if (user.Identity.IsAuthenticated)
+                {
 
+                }
+                // AuthStateProvider handles the case where the JWT is present but the user is not authenticated
+            }
+        }
     }
 }
