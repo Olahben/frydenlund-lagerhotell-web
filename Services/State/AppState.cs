3b7c6f7a8359831c60de559c2b46c20d565d7b6b
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-06-07 13:26:54 +0200

    Create

---
+++ Services/State/AppState.cs
@@ -0,0 +1,13 @@
+﻿namespace Lagerhotell.Services.State;
+public class AppState
+{
+    public bool IsUserLoggedIn { get; private set; }
+    public event Action OnChange;
+    public void NotifyStateChanged() => OnChange?.Invoke();
+
+    public void UserLoggedIn()
+    {
+        IsUserLoggedIn = true;
+        NotifyStateChanged();
+    }
+}
