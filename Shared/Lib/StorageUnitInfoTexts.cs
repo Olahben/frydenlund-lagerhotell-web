0fa1d6b2d1d27a55659a9ef0aee1900613784839
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-29 14:30:20 +0200

    Update

---
+++ Shared/Lib/StorageUnitInfoTexts.cs
@@ -0,0 +1,12 @@
+﻿namespace Lagerhotell.Shared.Lib
+{
+
+    public enum StorageUnitInfoTexts
+    {
+    "I dette lageret vil du kunne få plass til møbler og annet diverse fra en liten/normal enromsleilighet eller fra ett vanlig soverom.", //1-3M2
+    "I dette lageret vil du kunne få plass til innholdet til en liten normalt møblert leilighet inkludert noen andre eiendeler.", //4-8M2
+    "I dette lageret vil du kunne få plass til innholdet til en medium stor leilighet som er normalt møblert.", //9-12M2
+    "I dette lageret vil du kunne få plass til innholdet til en større leilighet som er normalt møblert.", //13-18M2
+    "I dette lageret vil du kunne få plass til innholdet til en større enmannsbolig som er normalt møblert i tillegg til fritidsutstyr.", //19-24M2
+    }
+}
