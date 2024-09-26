42d80cd276b608858ca5d94f46692689491ddf8c
Ola Hagen Bendixen <olaben@afk.no>
2024-09-26 13:45:15 +0200

    Configure auth0

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+    options.ProviderOptions.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("offline_access");
+});
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddScoped<SessionService>();
