2112e0454cec7f519c2182151f0b74b79302bc81
Ola Hagen Bendixen <olaben@afk.no>
2024-09-27 11:22:11 +0200

    use config corectly

---
+++ Services/Auth0Service.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Security.Cryptography;
+
+namespace Lagerhotell.Services;
+
+public class Auth0Service
+{
+    private readonly HttpClient client = new();
+    private readonly string _auth0Domain;
+    private readonly string _auth0FullDomain;
+    private readonly string _clientId;
+    private readonly string _clientSecret;
+    private readonly string _loginCallBackUrl;
+    private readonly SessionService _sessionService;
+    private readonly NavigationManager _navigationManager;
+
+    public Auth0Service(IConfiguration configuration, SessionService sessionService, NavigationManager navigationManager)
+    {
+        _auth0Domain = configuration["Auth0:Domain"];
+        _auth0FullDomain = $"https://{_auth0Domain}";
+        _clientId = configuration["Auth0:ClientId"];
+        _clientSecret = configuration["Auth0:ClientSecret"];
+        _loginCallBackUrl = $"https://localhost:5001{configuration["Auth0:LoginCallback"]}";
+        _sessionService = sessionService;
+        _navigationManager = navigationManager;
+    }
+
+    public string GenerateState()
+    {
+        var randomBytes = new byte[32];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomBytes);
+        }
+        return Convert.ToBase64String(randomBytes);
+    }
+
+    // Validates state in login callback to check if it is the same as the one generated here
+    public async Task RedirectToLoginPage()
+    {
+        string state = GenerateState();
+        _sessionService.AddLoginStateToLocalStorage(state);
+        string redirectUrl = $"https://{_auth0Domain}/authorize?response_type=code&client_id={_clientId}&redirect_uri={_loginCallBackUrl}&state={state}";
+        _navigationManager.NavigateTo(redirectUrl);
+    }
+}
