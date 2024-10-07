8ed58c6143b016b8adff2b42bf214214a9ab4570
Ola Hagen Bendixen <olaben@afk.no>
2024-10-07 15:01:08 +0200

    Run "GetAuthenticationStateAsync"

--- Pages/MyPage.cs
+++ Pages/MyPage.cs
@@ -15,22 +15,17 @@ namespace Lagerhotell.Pages
         protected NavigationManager NavigationManager { get; set; }
         [Inject]
         public SessionService SessionService { get; set; }
-        //[Inject]
-        // public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
         protected override async Task OnInitializedAsync()
         {
             var userJwt = await SessionService.GetJwtFromLocalStorage();
-            /*if (userJwt != null)
+            if (userJwt != null)
             {
                 var authState = await AuthenticationStateProvider
             .GetAuthenticationStateAsync();
-                var user = authState.User;
-                if (user.Identity.IsAuthenticated)
-                {
-
-                }
                 // AuthStateProvider handles the case where the JWT is present but the user is not authenticated
-            }*/
+            }
         }
     }
 }
